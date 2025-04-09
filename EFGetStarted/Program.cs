c68e4287b7c8d7c0c1d548fb809672e8e1319401
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-04-08 23:47:42 -0400

    Add Db migrations and Program.cs

--- EFGetStarted/Program.cs
+++ EFGetStarted/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using EFGetStarted.Models;
+using Microsoft.EntityFrameworkCore;
 
-Console.WriteLine("Hello, World!");
+using var db = new BloggingContext();
+
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+// Create
+Console.WriteLine("Inserting a new blog");
+db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+await db.SaveChangesAsync();
+
+// Read
+Console.WriteLine("Querying for a blog");
+var blog = await db.Blogs
+    .OrderBy(b => b.BlogId)
+    .FirstAsync();
+
+Console.WriteLine($"Found blog: {blog.Url}");
+
+// Update
+Console.WriteLine("Updating the blog and adding a post");
+blog.Url = "https://devblogs.microsoft.com/dotnet";
+blog.Posts.Add(new Post { Title = "Announcing .NET 10", Content = "More content" });
+await db.SaveChangesAsync();
+
+// Delete
+Console.WriteLine("Deleting the blog");
+db.Remove(blog);
+await db.SaveChangesAsync();
